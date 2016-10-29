3389529df2c2f3ff7698e944fe0f1403723cfbce
Patrick Cavanagh <patrickc@apadmi.com>
2016-10-29 12:55:22 +0100

    Empty project

---
+++ SwearJar/app/src/main/java/com/hackmanchester2016/swearjar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hackmanchester2016.swearjar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String PUBLIC_STATIC = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
