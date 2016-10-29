682095dd319552e6f4dd103f8ba584cf6862e6d1
Patrick Cavanagh <patrickc@apadmi.com>
2016-10-29 19:16:44 +0100

    Fixed google sign in
    YAAAASSSSSSS

--- SwearJar/app/src/main/java/com/hackmanchester2016/swearjar/LaunchActivity.java
+++ SwearJar/app/src/main/java/com/hackmanchester2016/swearjar/LaunchActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.firebase.ui.auth.AuthUI;
 import com.hackmanchester2016.swearjar.engine.Engine;
 import com.hackmanchester2016.swearjar.ui.launch.LaunchFragment;
 
@@ -17,13 +19,8 @@ public class LaunchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_launch);
 
-        showSignIn();
-    }
-
-    private void showSignIn() {
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.launch_fragment_container, new LaunchFragment(), LaunchFragment.TAG)
@@ -45,4 +42,5 @@ public class LaunchActivity extends AppCompatActivity {
             finish();
         }
     }
+
 }
--- SwearJar/app/src/main/java/com/hackmanchester2016/swearjar/ui/launch/LaunchFragment.java
+++ SwearJar/app/src/main/java/com/hackmanchester2016/swearjar/ui/launch/LaunchFragment.java
@@ -1,21 +1,29 @@
 package com.hackmanchester2016.swearjar.ui.launch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.firebase.ui.auth.AuthUI;
 import com.hackmanchester2016.swearjar.LaunchActivity;
 import com.hackmanchester2016.swearjar.R;
+import com.hackmanchester2016.swearjar.engine.Engine;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by dant on 29/10/2016.
  */
 
 public class LaunchFragment extends Fragment {
+
+    private static final int RC_SIGN_IN = 42;
 
     public static String TAG = "LAUNCH_FRAGMENT";
 
@@ -35,9 +43,29 @@ public class LaunchFragment extends Fragment {
     private Button.OnClickListener loginClickListener = new Button.OnClickListener() {
         @Override
         public void onClick(View view) {
-            if(getActivity() instanceof LaunchActivity) {
-                ((LaunchActivity)getActivity()).onSignIn();
-            }
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setProviders(AuthUI.GOOGLE_PROVIDER)
+                            .build(), RC_SIGN_IN);
         }
     };
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            Log.d(TAG, "user Id " + Engine.getInstance().getAuthManager().getUserId());
+
+            if(resultCode == RESULT_OK){
+                if(getActivity() instanceof LaunchActivity) {
+                    ((LaunchActivity)getActivity()).onSignIn();
+                }
+                Log.d(TAG, "Sign in successful");
+            } else{
+                Log.d(TAG, "Sign in failed");
+            }
+        }
+    }
 }
