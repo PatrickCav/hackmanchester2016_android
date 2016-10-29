7664fb19bd326a2be793b6436333cfb65794bbbf
Dan Tanner-Davies <Dant@apadmi.com>
2016-10-29 15:56:58 +0100

    Add sub fragments for View Pager (Setup)

---
+++ SwearJar/app/src/main/java/com/hackmanchester2016/swearjar/ui/setup/SetupFragmentLocation.java
@@ -0,0 +1,10 @@
+package com.hackmanchester2016.swearjar.ui.setup;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by dant on 29/10/2016.
+ */
+
+public class SetupFragmentLocation extends Fragment {
+}
